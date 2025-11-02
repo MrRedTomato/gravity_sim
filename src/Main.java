47db295777c4d758c7053a25ea02bedebb7e2d2a
alondoitel <alondoitel@gmail.com>
2025-11-02 12:25:53 -0800

    3 body system

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,25 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
-        int cellSize = 8;
-        int fps = 64;
+        int cellSize = 10;
+        int fps = 128;
 
         ArrayList<Mass> objects = new ArrayList<>();
 //        for (int i = 0; i < 2; i++) {
 //            Mass newObject = new Mass(Math.random() * 50, Math.random() * 50, Math.random());
 //            objects.add(newObject);
 //        }
-        objects.add(new Mass(25, 20, 0.01, 1.5));
-        objects.add(new Mass(50, 35, 100, 2));
-        objects.get(0).setXVel(3);
+        objects.add(new Mass(25, 32, 70, 1));
+        objects.add(new Mass(35, 37, 70, 1));
+        objects.get(0).setXVel(2);
+        objects.get(1).setXVel(-2);
         objects.get(0).setYVel(-2);
-//        objects.get(1).setXVel(-0.5);
+        objects.get(1).setYVel(2);
+
+        objects.add(new Mass(15, 20, 0.1, 1));
+        objects.get(2).setXVel(5);
+        objects.get(2).setYVel(-5);
+//        objects.get(2).setYVel(-1);
 
         Field field = new Field(objects);
         field.setStep(0.05);
